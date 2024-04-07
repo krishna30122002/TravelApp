fa8b88d7280206330f6ad1d5ae373cb4028fe39a
Abhishek <maurya4122002@gmail.com>
2024-04-07 18:28:14 +0530

    minr bug fixed

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import bookingRoute from './routes/booking.js'
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 const corsOptions ={
     origin:'https://mysimple-tour-travel.netlify.app', 
     credentials:true,            //access-control-allow-credentials:true
